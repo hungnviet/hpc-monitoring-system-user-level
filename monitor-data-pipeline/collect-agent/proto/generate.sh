a7c0a325bd4dcee730bf36bf0b8bebddc13c3461
hungnviet <nvh100904@gmail.com>
2025-12-28 10:54:20 +0700

    edit

--- monitor-data-pipeline/collect-agent/proto/generate.sh
+++ monitor-data-pipeline/collect-agent/proto/generate.sh
@@ -14,6 +14,9 @@ python3 -m grpc_tools.protoc \
     --grpc_python_out=. \
     alerts.proto
 
+sed -i 's/^import metrics_pb2/from . import metrics_pb2/' metrics_pb2_grpc.py
+sed -i 's/^import alerts_pb2/from . import alerts_pb2/' alerts_pb2_grpc.py
+
 echo "Generated proto files in collect-agent/proto/:"
 echo "  - metrics_pb2.py"
 echo "  - metrics_pb2_grpc.py"
